7689bec3ef6469774109d9502ffa5141396c6a2f
Roxana Moldovan <roxana.moldovan10@gmail.com>
2024-08-10 12:32:37 +0200

    Update repository name

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
-    base: '/{basic-search}',
+    base: '/basic-search',
     plugins: [react()],
 });
